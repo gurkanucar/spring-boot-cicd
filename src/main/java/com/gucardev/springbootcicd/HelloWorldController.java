a9e5e0b53e2a03e711e0b7e1a190f5a0b8d498a3
Gurkan UCAR <ucar.gurkan@hotmail.com>
2026-05-19 15:32:27 +0300

    add api

--- src/main/java/com/gucardev/springbootcicd/HelloWorldController.java
+++ src/main/java/com/gucardev/springbootcicd/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.gucardev.springbootcicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 
 }
